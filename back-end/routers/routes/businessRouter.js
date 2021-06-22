d324a2b1ab120fc4a3b38d603dfeae161187beff
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-06-22 21:12:14 +0300

    getBusinessById function done + modifications to other functions

--- back-end/routers/routes/businessRouter.js
+++ back-end/routers/routes/businessRouter.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const businessRouter = express.Router();
 
-const {addBusiness,updateBusiness,deleteBusiness,getBusinessByType} = require('./../../businessController')
+const {addBusiness,updateBusiness,deleteBusiness,getBusinessByType,getBusinessById} = require('./../../businessController')
 
 businessRouter.post('/',addBusiness);
 businessRouter.put('/:business_id', updateBusiness);
 businessRouter.delete('/:business_id',deleteBusiness);
-businessRouter.get('/type/:type', getBusinessByType)
+businessRouter.get('/type/:type', getBusinessByType);
+businessRouter.get('/id/:id',getBusinessById);
 
 module.exports = businessRouter;
