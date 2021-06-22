e3d3c03f2a81f2daccca8744042675c47cd064f8
ferasdiab <dfiras96@gmail.com>
2021-06-22 18:45:29 +0300

    add router for all back end

--- back-end/main.js
+++ back-end/main.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-
+const cors = require("cors");
 const db = require('./db/db');
 
+app.use(express.json());
+
+app.use(cors());
+
 /* ==================== */
+const roleRouter = require("./routers/routes/roleRouter")
+const usersRouter = require("./routers/routes/usersRouter")
+const loginRouter = require("./routers/routes/loginRouter")
+const businessRouter = require("./routers/routes/businessRouter")
+const imageRouter = require("./routers/routes/imageRouter")
+const commentsRouter = require("./routers/routes/commentsRouter")
+const ratingRouter = require("./routers/routes/ratingRouter")
+
+/* ==================== */
+app.use("/role",roleRouter)
+app.use("/users",usersRouter)
+app.use("/login",loginRouter)
+app.use("/business",businessRouter)
+app.use("/image",imageRouter)
+app.use("/comments",commentsRouter)
+app.use("/rating",ratingRouter)
 
 
 /* ==================== */
-
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log('SERVER IS WORKING ON http://localhost:' + PORT);
