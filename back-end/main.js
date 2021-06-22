aabf454053c276e8ffbc8563046ada8b9fca22d7
ferasdiab <dfiras96@gmail.com>
2021-06-22 17:09:58 +0300

    starter code for back end

--- back-end/main.js
+++ back-end/main.js
@@ -2,20 +2,14 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const db = require('./db');
-
-const users = require('./users');
-/* ==================== */
-
-// console.log('HHHHHHHHHHHHHHHHHHHHHHHHHH:',users);
-// users.findAll();
-
-app.get('/users', users.findAll);
-app.get('/users/email', users.findEmail);
+const db = require('./db/db');
 
 /* ==================== */
 
-const PORT = 3000;
+
+/* ==================== */
+
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log('SERVER IS WORKING ON http://localhost:' + PORT);
 });
