8f8ecaccc5214f0d2f1e2444012ce6ae27961986
ferasdiab <dfiras96@gmail.com>
2021-06-23 11:49:50 +0300

    fixErrorSend

--- back-end/routers/routes/ratingRouter.js
+++ back-end/routers/routes/ratingRouter.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const ratingRouter = express.Router();
 
-const { addRating } = require('./../controllers/rating');
+const { addRating ,getUserRating} = require('./../controllers/rating');
 const { authentication } = require("../middlewares/authentication");
 
 
 ratingRouter.post('/:business_id',authentication,addRating)
+ratingRouter.get('/:business_id',authentication,getUserRating)
 
 module.exports = ratingRouter;
