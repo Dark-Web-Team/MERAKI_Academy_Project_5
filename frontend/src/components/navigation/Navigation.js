c6cedd4eee407f279d598cc049842c8245777afe
ferasdiab <dfiras96@gmail.com>
2021-06-25 15:29:35 +0300

    addSomeFunForNav

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link ,useHistory} from 'react-router-dom';
-
+import {useDispatch, useSelector} from 'react-redux';
+import {setToken} from "../../reducers/login"
 import {Navbar,Button,Nav,Form,FormControl,NavDropdown} from "react-bootstrap"
 import "./navigation.css";
 
 const Navigation = () => {
+  const dispatch = useDispatch()
 	const history = useHistory();
+  const state = useSelector((state) => {
+    return {
+      
+      token: state.login.token,
+    };
+  });
+
+  
+
+
 
 	return <div className="nav-container">
 		<Navbar bg="dark" variant="dark">
@@ -15,20 +27,23 @@ const Navigation = () => {
           history.push("/");
 
 	  }} >  Home</Nav.Link>
-      <Nav.Link onClick={()=>{
+      {!state.token ? <Nav.Link onClick={()=>{
           history.push("/login");
 
-	  }} >Login</Nav.Link>
-      <Nav.Link onClick={()=>{
-          history.push("/signUp");
+	  }} >Login</Nav.Link> :"" }
+      {state.token  ? <Nav.Link onClick={()=>{
+         localStorage.clear()
+         dispatch(setToken(""));
 
-	  }}>sign Up</Nav.Link>
+
+	  }}>sign out</Nav.Link>:""}
     </Nav>
     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-info">Search</Button>
     </Form>
   </Navbar>
+  
 	</div>
 };
 
