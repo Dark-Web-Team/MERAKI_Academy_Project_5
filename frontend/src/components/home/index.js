a2e0326d16eae029569a84cfb14202c0ec5ddd2c
LoayIssa <issaloay680@gmail.com>
2021-06-28 18:31:53 +0300

    css footer

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -68,7 +68,6 @@ export default function Home() {
           <p>Contact Us</p>
           <p id="numper_email">055450500</p>
           <p id="numper_email">Dark-Web@Dark-Web.com</p>
-          <p></p>
 
 
           
