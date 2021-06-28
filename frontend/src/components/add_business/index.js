8e281304410b6104248d7be2758cf134a6e21ddd
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-06-28 10:59:21 +0300

    added backend request to add_business

--- frontend/src/components/add_business/index.js
+++ frontend/src/components/add_business/index.js
@@ -1,96 +1,165 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import Form  from "react-bootstrap/Form";
-import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import { useParams } from "react-router-dom";
-import "./add_business.css"
+import "./add_business.css";
+import Alert from "react-bootstrap/Alert";
 
-const AddBusiness = ()=>{
-    const [displayName, setDisplayName] = useState('');
-    const [description, setDescription] = useState('');
-    const [bookingPrice, setBookingPrice] = useState(0);
-    const [city, setCity] = useState('');
-    const [openingTime, setOpeningTime] = useState('');
-    const [closingTime, setClosingTime] = useState('');
-    const [type, setType] = useState('');
-    const [main_img, setMain_img] = useState('')
+const AddBusiness = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [description, setDescription] = useState("");
+  const [bookingPrice, setBookingPrice] = useState(0);
+  const [city, setCity] = useState("");
+  const [openingTime, setOpeningTime] = useState("");
+  const [closingTime, setClosingTime] = useState("");
+  const [type, setType] = useState("");
+  const [main_img, setMain_img] = useState("");
+  const [errMessage, setErrMessage] = useState("");
+  const [errPresent, setErrPresent] = useState(false);
 
-    const {id} = useParams();
+  const { id } = useParams();
 
-    return(<>
-    <div className='addBusinss'>
+  const handleSubmit = async () => {
+    try {
+      const addConfirm = await axios.post(
+        `${process.env.REACT_APP_BACKEND_SERVER}business`,
+        {
+          displayName,
+          description,
+          bookingPrice,
+          city,
+          openingTime,
+          closingTime,
+          type,
+          main_img,
+          owner_id: id,
+        }
+      );
+      console.log(addConfirm)
+    } catch (err) {
+      console.log(err.response);
+      setErrPresent(true);
+      setErrMessage(err.response.data);
+    }
+  };
+
+  return (
+    <>
+      <div className="addBusinss">
         <Form className="addForm">
-            <Form.Group>
-                <Form.Label>Business Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter business name" onChange={(e)=>{
-        setDisplayName(e.target.value)
-    }} />
-            </Form.Group>
-            <Form.Group>
+          <Form.Group>
+            <Form.Label>Business Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter business name"
+              onChange={(e) => {
+                setDisplayName(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
             <Form.Label>Select Business Type</Form.Label>
-    <Form.Control as="select" onChange={(e)=>{
-        setType(e.target.value)
-    }}>
-      <option value="sport">sport</option>
-      <option value="indoor_entertainment">indoor_entertainment</option>
-      <option value="indoor_entertainment">indoor_entertainment</option>
-    </Form.Control>
-  </Form.Group>
-            <Form.Group>
-    <Form.Label>Select City</Form.Label>
-    <Form.Control as="select" onChange={(e)=>{
-        setCity(e.target.value)
-    }}>
-      <option value="amman">amman</option>
-      <option value="zarqa">zarqa</option>
-      <option value="irbid">irbid</option>
-      <option value="salt">salt</option>
-      <option value="karak">karak</option>
-      <option value="jarash">jarash</option>
-      <option value="ajloun">ajloun</option>
-      <option value="mafraq">mafraq</option>
-      <option value="ma'an">ma'an</option>
-      <option value="tafeleih">tafeleih</option>
-      <option value="madaba">madaba</option>
-      <option value="aqaba">aqaba</option>
-    </Form.Control>
-  </Form.Group>
-            <Form.Group>
-                <Form.Label>Booking Price</Form.Label>
-                <Form.Control type="number" placeholder="Enter Booking Price" onChange={(e)=>{
-        setBookingPrice(e.target.value)
-    }} />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Opening Time</Form.Label>
-                <Form.Control type="text" placeholder="Enter Opening Time" onChange={(e)=>{
-        setOpeningTime(e.target.value)
-    }} />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Closing Time</Form.Label>
-                <Form.Control type="text" placeholder="Enter Closing Time" onChange={(e)=>{
-        setClosingTime(e.target.value)
-    }} />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Main Business Image</Form.Label>
-                <Form.Control type="text" placeholder="Enter Main Image" onChange={(e)=>{
-        setMain_img(e.target.value)
-    }} />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Description of Business</Form.Label>
-              <Form.Control as="textarea" rows={3} onChange={(e)=>{
-        setDescription(e.target.value)
-    }} />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+            <Form.Control
+              as="select"
+              onChange={(e) => {
+                setType(e.target.value);
+              }}
+            >
+              <option value="sport">Sport</option>
+              <option value="entertainment">Entertainment</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Select City</Form.Label>
+            <Form.Control
+              as="select"
+              onChange={(e) => {
+                setCity(e.target.value);
+              }}
+            >
+              <option value="amman">Amman</option>
+              <option value="zarqa">Zarqa</option>
+              <option value="irbid">Irbid</option>
+              <option value="salt">Salt</option>
+              <option value="karak">Karak</option>
+              <option value="jarash">Jarash</option>
+              <option value="ajloun">Ajloun</option>
+              <option value="mafraq">Mafraq</option>
+              <option value="ma'an">Ma'an</option>
+              <option value="tafeleih">Tafeleih</option>
+              <option value="madaba">Madaba</option>
+              <option value="aqaba">Aqaba</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Booking Price</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter Booking Price"
+              onChange={(e) => {
+                setBookingPrice(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Opening Time</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Opening Time"
+              onChange={(e) => {
+                setOpeningTime(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Closing Time</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Closing Time"
+              onChange={(e) => {
+                setClosingTime(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Main Business Image</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Main Image"
+              onChange={(e) => {
+                setMain_img(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Description of Business</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+          </Form.Group>
         </Form>
-    </div>
-    </>)
-}
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Submit
+          </Button>
+          {errPresent && (
+            <div>
+              <br />{" "}
+              <Alert
+                variant="danger"
+                style={{ width: "35vw", textAlign: "center" }}
+              >
+                {errMessage}
+              </Alert>
+            </div>
+          )}
+      </div>
+    </>
+  );
+};
 
-export default AddBusiness
+export default AddBusiness;
--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -4,11 +4,27 @@ import jwt from "jsonwebtoken"
 import { Button } from "react-bootstrap";
 import axios from "axios";
 import "./profile.css";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
   const [userInfo, setUserInfo] = useState("");
   let thisToken = localStorage.getItem("token");
   const history = useHistory();
+  const token = useSelector(state => state.login.token)
+  let userId = ""
+
+  const getUserId = async ()=>{
+        const token1= token.split(' ').pop();
+        try{
+        const tokenPayload = await jwt.decode(token1);
+        console.log(tokenPayload)
+        userId = tokenPayload.user_id 
+        history.push(`/profile/${userId}/addBusiness`)
+        } catch(err){
+            console.log(err);
+            throw err
+        }
+  }
 
 
   useEffect(() => {
@@ -57,7 +73,7 @@ export default function Profile() {
               </p>
             </div>
            <div> <Button id="editButton" onClick={()=>{history.push("edit-profile")}} >edit profile</Button> 
-           <Button id="addBusinessButton" onClick={()=>{history.push("/3/addBusiness")}} >Add a business</Button>
+           <Button id="addBusinessButton" onClick={getUserId} >Add a business</Button>
            </div>
           </div>
         </div>
