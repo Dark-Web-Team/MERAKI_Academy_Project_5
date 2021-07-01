af57a115292c9064e69bfe5e0db5be1fe1ec855f
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-07-01 20:13:11 +0300

    add card functionailty + style changes to payment page

--- frontend/src/components/payment/index.js
+++ frontend/src/components/payment/index.js
@@ -3,13 +3,12 @@ import axios from "axios";
 import Cards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
 import "./payment.css";
-import { FormControl,Form} from "react-bootstrap";
- import {FaCcVisa,FaCreditCard} from 'react-icons/fa'
- import {GiPayMoney} from 'react-icons/gi'
- import {FaCcApplePay} from 'react-icons/fa'
- import {  useSelector } from "react-redux";
- import { useParams } from "react-router-dom";
-
+import {  Form } from "react-bootstrap";
+import { FaCcVisa, FaCreditCard } from "react-icons/fa";
+import { GiPayMoney } from "react-icons/gi";
+import { FaCcApplePay } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 export default function Payment() {
   let thisToken = localStorage.getItem("token");
@@ -20,60 +19,90 @@ export default function Payment() {
   const [expiry, setExpiry] = useState("");
   const [cvc, setCvc] = useState("");
   const [focused, setFocused] = useState("");
-  const [pay, setPay] = useState(false)
+  const [pay, setPay] = useState(false);
   const { busnisses_id } = useParams();
+  const [saveCard, setSaveCard] = useState(false);
+
+
   const state = useSelector((state) => {
     return {
       reservation_date: state.reservation.reservation_date,
       reservation_time: state.reservation.reservation_time,
-      token:state.login.token
+      token: state.login.token,
     };
   });
 
-  const reservation = ()=> {
-    axios.post(
-      `http://localhost:5000/reservations/${busnisses_id}`,
-      { reservation_date:state.reservation_date, reservation_time:state.reservation_time },
-      {
-        headers: {
-          authorization: "Bearer " + state.token,
+  const reservation = () => {
+    axios
+      .post(
+        `http://localhost:5000/reservations/${busnisses_id}`,
+        {
+          reservation_date: state.reservation_date,
+          reservation_time: state.reservation_time,
         },
-      }
-    ).then(result=>{
-      console.log("result",result.data);
-    }).catch((err) => {
-      console.log(err);
-    });
+        {
+          headers: {
+            authorization: "Bearer " + state.token,
+          },
+        }
+      )
+      .then((result) => {
+        console.log("result", result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-    axios.post(`${process.env.REACT_APP_BACKEND_SERVER}sendEmail`, {
-      date:state.reservation_date,
-      time:state.reservation_time
-    },
-   {
-      headers: {
-      authorization: "Bearer " + state.token,
-    },
-  }
-    )
-    .then((result) => {
-      console.log("sent successfully");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
-  
+    axios
+      .post(
+        `${process.env.REACT_APP_BACKEND_SERVER}sendEmail`,
+        {
+          date: state.reservation_date,
+          time: state.reservation_time,
+        },
+        {
+          headers: {
+            authorization: "Bearer " + state.token,
+          },
+        }
+      )
+      .then((result) => {
+        console.log("sent successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+      if(saveCard){
+        axios.post(`${process.env.REACT_APP_BACKEND_SERVER}creditCards`,{
+          cardNumber: number,
+          cardHolder: name,
+          expiryDate: expiry
+        },
+        {
+          headers: {
+            authorization: "Bearer " + state.token,
+          },
+        })
+        .then((response)=>{
+          console.log('card added')
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+      }
+  };
+
   /*
     
     */
   return (
     <>
-
-        <div className="h4">
-          <h4>inter your credit Card details to continue with your payment</h4>
-        </div>
+      <div className="h4">
+        <h4>inter your credit Card details to continue with your payment</h4>
+      </div>
       <div className="parant-payment">
-      < div className = "cardchldren">
+        <div className="cardchldren">
           <Cards
             number={number}
             name={name}
@@ -81,94 +110,88 @@ export default function Payment() {
             cvc={cvc}
             focused={focused}
           />
-          
         </div>
-        
-        <div className="payment-input">
-          <div>
-          <FormControl
-          name="number"
-          placeholder="number"
-          aria-label="Large"
-          aria-describedby="inputGroup-sizing-sm"
-          onChange={(e) => {
-            setNumber(e.target.value);
-          }}
-          onFocus={(e) => {
-            setFocused(e.target.name);
-          }}
-        />
-            
-          </div>
-          <div>
 
-         <FormControl
-          name="name"
-          placeholder="Name"
-          aria-label="Large"
-          aria-describedby="inputGroup-sizing-sm"
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          onFocus={(e) => {
-            setFocused(e.target.name);
-          }}
-        />
-          </div>
-          <div>
-            
-
-           <FormControl
-          name="expiry"
-          placeholder="MM/YY expiry"
-          aria-label="Large"
-          aria-describedby="inputGroup-sizing-sm"
-          onChange={(e) => {
-            setExpiry(e.target.value);
-          }}
-          onFocus={(e) => {
-            setFocused(e.target.name);
-          }}
-        />
-          </div>
-          <div>
-          <FormControl
-          name="cvc"
-          placeholder="Cvc"
-          aria-label="Large"
-          aria-describedby="inputGroup-sizing-sm"
-          onChange={(e) => {
-            setCvc(e.target.value);
-          }}
-          onFocus={(e) => {
-            setFocused(e.target.name);
-          }}
+        <Form className="payment-input">
+          <Form.Group>
+            <Form.Control
+              type='text'
+              name="number"
+              placeholder="number"
+              aria-label="Large"
+              aria-describedby="inputGroup-sizing-sm"
+              onChange={(e) => {
+                setNumber(e.target.value);
+              }}
+              onFocus={(e) => {
+                setFocused(e.target.name);
+              }}
             />
-            <Form.Group >
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-
-          </div>
-
-          
-        </div>
-        
-        
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type='text'
+              name="name"
+              placeholder="Name"
+              aria-label="Large"
+              aria-describedby="inputGroup-sizing-sm"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              onFocus={(e) => {
+                setFocused(e.target.name);
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type='text'
+              name="expiry"
+              placeholder="MM/YY expiry"
+              aria-label="Large"
+              aria-describedby="inputGroup-sizing-sm"
+              onChange={(e) => {
+                setExpiry(e.target.value);
+              }}
+              onFocus={(e) => {
+                setFocused(e.target.name);
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type='text'
+              name="cvc"
+              placeholder="Cvc"
+              aria-label="Large"
+              aria-describedby="inputGroup-sizing-sm"
+              onChange={(e) => {
+                setCvc(e.target.value);
+              }}
+              onFocus={(e) => {
+                setFocused(e.target.name);
+              }}
+            />
+            <Form.Group>
+              <Form.Check type="checkbox" label="Save Credit Card" onChange={()=>{
+                console.log(saveCard)
+                setSaveCard(!saveCard)
+              }} />
+            </Form.Group>
+          </Form.Group>
+        </Form>
       </div>
 
-    
-      <div className = "button">
-          <button onClick={reservation} >Confirm Payment</button>
-          {/* {!pay?<button onClick={payNow}>Confirm Payment</button>:<div>Payment done</div>} */}
-          
-        </div>
-        <div className = "iconspayment">
-         < FaCcVisa className="icons"/>
-         <FaCreditCard className="icons"/>
-         <GiPayMoney className="icons"/>
-        <FaCcApplePay className="icons"/>
-       </div>
-        
+      <div className="button">
+        <button onClick={reservation}>Confirm Payment</button>
+        {/* {!pay?<button onClick={payNow}>Confirm Payment</button>:<div>Payment done</div>} */}
+      </div>
+      <div className="iconspayment">
+        <FaCcVisa className="icons" />
+        <FaCreditCard className="icons" />
+        <GiPayMoney className="icons" />
+        <FaCcApplePay className="icons" />
+      </div>
     </>
   );
 }
