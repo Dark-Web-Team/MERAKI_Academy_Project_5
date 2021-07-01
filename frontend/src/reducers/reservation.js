4c143ab408e7e0bf344dae25eccc166482a88e2d
LoayIssa <issaloay680@gmail.com>
2021-07-01 10:48:01 +0300

    add reducers reservation

--- frontend/src/reducers/reservation.js
+++ frontend/src/reducers/reservation.js
@@ -15,11 +15,8 @@ const initialState = {
       case "SET_RESERVATION":
         return { reservation_date: payload.reservation_date ,reservation_time:payload.reservation_time};
   
-       
-  
       default:
         return state;
     }
   };
-
   export default reservation;
