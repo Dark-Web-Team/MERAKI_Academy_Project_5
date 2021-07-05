39fd25fb87cab1968fc284abf77bc1845872c081
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-07-05 17:35:07 +0300

    finished payment confirm

--- frontend/src/components/payment_confirmed/index.js
+++ frontend/src/components/payment_confirmed/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import "./donation_confirmed.css";
+import "./payment_confirmed.css";
 import { useHistory } from "react-router";
+import Button from "react-bootstrap/Button"
 
 const PaymentConfirm = ()=>{
     const history=useHistory()
     return (<>
     <div className="donateConfirm">
         <div className="midBox">
-        <p className="text1">Thank You For Your Donation</p>
-        <button className="homeButton" onClick={()=>{history.push("/")}}>Go to home</button>
+        <p className="text1">Reservation confirmed</p>
+        <p className='text2'>Thank you for using our Services</p>
+        <Button variant="outline-info" size="lg" onClick={()=>{history.push("/")}}>
+            Go to home
+        </Button>
         </div>
     </div>
     </>)
