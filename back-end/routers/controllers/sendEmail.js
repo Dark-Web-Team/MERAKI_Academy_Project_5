9218ef4f2888b017720de828e01a42719802f95f
ferasdiab <dfiras96@gmail.com>
2021-07-11 11:56:11 +0300

    add reminder message

--- back-end/routers/controllers/sendEmail.js
+++ back-end/routers/controllers/sendEmail.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+var cron = require('node-cron');
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -10,6 +11,18 @@ const transporter = nodemailer.createTransport({
 
 const Transporter = (req, res) => {
   const { time , date} = req.body;
+  let month = parseInt(date.slice(5,7))
+  let day  = parseInt(date.slice(8,10))
+  if (day-1 === 0){
+    month = month -1 
+    day = 30 
+
+  }else {
+    day = day-1
+  }
+
+
+
   const to = req.token.email
   
   const mailOptions = {
@@ -26,6 +39,21 @@ const Transporter = (req, res) => {
 
     BookingWebsite team`,
   };
+
+  const mailOptionsRemind = {
+    from: "bookingwebsitee@gmail.com",
+    to: to,
+    subject: "Reservation Reminder",
+    text: `Hello ,
+    
+    We want to remind you about  your booking  on ${date} at ${time}
+
+    please be there 10 minutes before reservation time 
+    
+    Best wishes,
+
+    BookingWebsite team`,
+  };
   
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
@@ -35,6 +63,19 @@ const Transporter = (req, res) => {
     //   console.log("Email sent: " + info.response);
     }
   });
+
+  cron.schedule(`* * ${day} ${month} * `, () => {
+    transporter.sendMail(mailOptionsRemind, function (error, info) {
+      if (error) {
+        console.log(error);
+      } 
+    });
+  });
+  
+
+
+
+
 };
 
 module.exports = {
